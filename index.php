5fb75b34fb35957cc1b453ddd49c5f3a7024da56
cokeIII <coke2484@gmail.com>
2021-09-15 12:46:21 +0700

    first

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php require_once "setHead.php"; ?>
+</head>
+
+<body>
+    <div class="d-flex" id="wrapper">
+        <!-- Sidebar-->
+        <?php require_once "menuSidebar.php";?>
+        <!-- Page content wrapper-->
+        <div id="page-content-wrapper">
+            <!-- Top navigation-->
+            <?php require_once "menuTop.php";?>
+            <!-- Page content-->
+            <div class="container-fluid">
+                
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
+<?php require_once "setFoot.php"; ?>
