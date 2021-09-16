56f53166551bfb4e3f6bd8f19a56b21cda0c1acf
cokeIII <coke2484@gmail.com>
2021-09-16 20:11:12 +0700

    t

--- connect.php
+++ connect.php
@@ -5,7 +5,7 @@ $password = "chontech2020!";
 $db = "vaccinate";
 // Create connection
 $conn = new mysqli($servername, $username, $password,$db);
-
+$conn->set_charset("utf8");
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
