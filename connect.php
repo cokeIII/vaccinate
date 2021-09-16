425041a9e2abdc4dff6d62e0b228516ad9a21af4
cokeIII <coke2484@gmail.com>
2021-09-16 20:09:34 +0700

    t

--- connect.php
+++ connect.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "";
+$password = "chontech2020!";
 $db = "vaccinate";
 // Create connection
 $conn = new mysqli($servername, $username, $password,$db);
