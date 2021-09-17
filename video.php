55f901ce45d58b7893ebd1e96b491766507bc7b0
worakitw <worakitw@gmail.com>
2021-09-17 19:26:52 +0700

    video

---
+++ video.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php require_once "setHead.php"; ?>
+</head>
+
+<body>
+    <div id="page-content-wrapper">
+        <div class="container-fluid">
+            <div class="text-center text-primary">
+                <h4>เตรียมความพร้อมเปิดภาคเรียนที่ 2/2564 สถานศึกษาปลอดภัย เด็กได้รับวัคซีนถ้วนหน้า
+                <h5 class="text-danger"><b> ทำความเข้าใจกับ video ทั้ง 4 ชุดก่อน จึงจะทำรายการต่อไปได้</b></h5> 
+            </div>
+            
+            <div class="row">
+                <iframe width="1077" height="480" src="https://www.youtube.com/embed/KWJfL-kzLKA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <br>
+                <iframe width="1077" height="480" src="https://www.youtube.com/embed/-9C-isSnH9s" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <br>
+                <iframe width="1077" height="480" src="https://www.youtube.com/embed/K3vWwl6w23g" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <br>
+                <iframe width="1077" height="480" src="https://www.youtube.com/embed/W2gyAyuSYYY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+
+        </div>
+    </div>
+
+</body>
+
+</html>
+<?php require_once "setFoot.php"; ?>
