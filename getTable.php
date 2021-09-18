0686e8a8898776a37148ca20cf68705334ffaea1
cokeIII <coke2484@gmail.com>
2021-09-18 21:06:00 +0700

    f

---
+++ getTable.php
@@ -0,0 +1,45 @@
+<?php
+header('Content-Type: text/html; charset=UTF-8');
+require_once "connect.php";
+require_once "function.php";
+$datalist = array();
+if (!empty($_POST["group_id"])) {
+    $group_id = $_POST["group_id"];
+    $sql = "select *, d.status as docStatus from stu_status ss
+    left join student s on ss.student_id = s.student_id
+    inner join doc2 d on d.student_id = ss.student_id
+    inner join prefix p on p.prefix_id = s.perfix_id 
+    where s.group_id = '$group_id'";//632090101
+} else {
+    $sql = "select *, d.status as docStatus from stu_status ss
+    left join student s on ss.student_id = s.student_id
+    inner join doc2 d on d.student_id = ss.student_id
+    inner join prefix p on p.prefix_id = s.perfix_id
+    ";
+}
+$datalist["data"][0]["std_id"] = "ไม่มีข้อมูล";
+$datalist["data"][0]["std_name"] = "";
+$datalist["data"][0]["stu_status"] = "";
+$datalist["data"][0]["age"] = "";
+$datalist["data"][0]["phone_std"] = "";
+$datalist["data"][0]["time_stamp"] = "";
+$datalist["data"][0]["status"] = "";
+// $datalist["data"][0]["print"] = "";
+$datalist["data"][0]["btnPrint"] = "";
+
+
+$res = mysqli_query($conn, $sql);
+$i = 0;
+while ($row = mysqli_fetch_assoc($res)) {
+    $datalist["data"][$i]["std_id"] = $row["student_id"];
+    $datalist["data"][$i]["std_name"] = $row["prefix_name"] . $row["stu_fname"] . " " . $row["stu_lname"];
+    $datalist["data"][$i]["stu_status"] = $row["student_status"];
+    $datalist["data"][$i]["age"] = calAgeV2($row["birthday"])[0];
+    $datalist["data"][$i]["phone_std"] = $row["phone_std"];
+    $datalist["data"][$i]["time_stamp"] = $row["time_stamp"];
+    $datalist["data"][$i]["status"] = '<button class="btn '.($row["docStatus"]=="ส่งแล้ว"?"btn-secondary":"btn-success").' updatePrint" stdId="' . $row["student_id"] . '">'.$row["docStatus"].'</button>';
+    // $datalist["data"][$i]["print"] = '<button class="btn btn-success updatePrint" stdId="' . $row["student_id"] . '"><i class="fas fa-clipboard-list"></i></button>';
+    $datalist["data"][$i]["btnPrint"] = '<button class="btn btn-success print" stdId="' . $row["student_id"] . '"><i class="fas fa-print"></i></button>';
+    $i++;
+}
+echo json_encode($datalist, JSON_UNESCAPED_UNICODE);
---
+++ updatePrint.php
@@ -0,0 +1,14 @@
+<?php
+require_once "connect.php";
+$student_id = $_POST["student_id"];
+$status = $_POST["status"];
+if($status == "พิมพ์แล้ว"){
+    $status = "ส่งแล้ว";
+} else {
+    $status = "พิมพ์แล้ว";
+}
+$sql = "update doc2 set status = '$status' where student_id = '$student_id'";
+$res = mysqli_query($conn,$sql);
+if(mysqli_affected_rows($conn) > 0){
+    echo "ok";
+}
