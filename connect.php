fb66288c0262a1964c19fa1b55f22a3b09224102
cokeIII <52317579+cokeIII@users.noreply.github.com>
2022-09-23 07:18:09 +0700

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "chontech2020!";
+$password = "";
 $db = "vaccinate";
 // Create connection
 $conn = new mysqli($servername, $username, $password,$db);
